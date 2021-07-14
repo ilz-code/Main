792df367ad245709907ab593fa16411ad86fd6da
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-07-14 21:09:36 +0300

    Exc 8

---
+++ TypesAndVariables/8_MinutesToYearsAndDates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _8_MinutesToYearsAndDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter large number");
+            int input = Convert.ToInt32(Console.ReadLine());
+            double days = input / (24 * 60);
+            double years = days / 365;
+            int daysInt = Convert.ToInt32(days);
+            int yearsInt = Convert.ToInt32(Math.Floor(years));
+            int daysLeft = daysInt - yearsInt * 365;
+            Console.WriteLine($"{input} minutes corresponds to {yearsInt} years and {daysLeft} days.");
+            Console.ReadKey();
+        }
+    }
+}
