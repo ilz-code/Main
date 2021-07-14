d7cef6e7f52a7628c016b67932dd24fdecf1eb30
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-07-14 07:34:26 +0300

    Add files via upload

---
+++ TypesAndVariables/Casting/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            First();
+            Second();
+            Console.ReadKey();
+        }
+
+        static void First()
+        {
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            double d = 4;
+            float e = 5;
+            
+            int sum = a + b + c + (int)d + (int)e;
+            Console.WriteLine(sum);
+        }
+
+        static void Second()
+        {
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            double d = 4.2;
+            float e = 5.3f;
+
+            double sum = Math.Round( a + b + c + d + e, 1);
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ TypesAndVariables/SadFor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SadFor
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Test1();
+            Console.ReadKey();
+        }
+
+        private static void Test1()
+        {
+            
+            for ( int i = 0; i < 10; i++)
+            {
+                if (Math.Sqrt(i) > 2.5)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+           
+        }
+        
+    }
+}
