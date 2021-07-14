cb60ed7f609594ed7b83709d641eca85f7a52b6f
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-07-14 20:17:15 +0300

    Exc 6

---
+++ TypesAndVariables/6_Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _6_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter some one-digit numbers");
+            string input = Console.ReadLine();
+            char[] charArr = input.ToCharArray();
+            int[] numArr = new int[input.Length];
+            int sum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                numArr[i] = charArr[i];
+                sum += numArr[i] -48;
+                Console.WriteLine($"{i}-tais char ir {charArr[i]}, cipars {numArr[i]}, summa {sum} ");
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
