7f7e2c5c4610a5b1ecf3e8c1759c00b815f886fb
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-07-14 16:33:58 +0300

    Exercise 4

---
+++ TypesAndVariables/4-NameAndYear/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_NameAndYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter year of birth");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"My name is {name} and I was born in {year}.");
+            Console.ReadKey();
+        }
+    }
+}
