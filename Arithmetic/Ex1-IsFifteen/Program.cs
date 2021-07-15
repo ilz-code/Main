43847d90be80d952891ea872262b65bbdfc9ef45
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-07-15 22:18:20 +0300

    Ex1

---
+++ Arithmetic/Ex1-IsFifteen/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1_IsFifteen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter two numbers");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            
+            bool isFifteen;
+            if (num1 == 15)
+                isFifteen = true;
+            else if (num2 == 15)
+                isFifteen = true;
+            else if (num1 + num2 == 15)
+                isFifteen = true;
+            else if (num1 - num2 == 15)
+                isFifteen = true;
+            else if (num2 - num1 == 15)
+                isFifteen = true;
+            else
+                isFifteen = false;
+            Console.WriteLine(isFifteen);
+            Console.ReadKey();
+        }
+    }
+}
