1a6be5bc8b86637e7f9a698c2a2c476b6033ec45
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-07-15 23:19:23 +0300

    Ex 4, 5

---
+++ Arithmetic/Ex5-guessNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ex5_guessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(100);
+            Console.WriteLine("Guess number from 1 to 100");
+            int guess = Convert.ToInt32(Console.ReadLine());
+            if (guess == number)
+                Console.WriteLine("You get right!");
+            else if (guess < number)
+                Console.WriteLine("Too low!");
+            else if (guess > number)
+                Console.WriteLine("Too high!");
+            else
+                Console.WriteLine("Wrong input");
+            Console.ReadKey();
+        }
+    }
+}
