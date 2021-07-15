ded396bad24841d71d29d84c4b8f5082da7a6a79
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-07-15 22:45:59 +0300

    Ex 2, 3

---
+++ Arithmetic/Ex2-CheckOddEven/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex2_CheckOddEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+                Console.WriteLine("Even Number");
+            else
+                Console.WriteLine("Odd Number");
+            Console.WriteLine("bye!");
+            Console.ReadKey();
+        }
+    }
+}
