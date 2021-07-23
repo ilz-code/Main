40aae63095cabc4ad46d45b918e5aa28b631942c
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-07-23 20:52:02 +0300

    Ex7-10

---
+++ Loops/Loops/Exercise7_Piglet/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise7_Piglet
+{
+    class Piglet
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Piglet!");
+            var rnd = new Random();
+            int roll = 0;
+            string ans = "";
+            int sum = 0;
+            do
+            {
+                roll = rnd.Next(9);
+                if (roll == 1)
+                {
+                    Console.WriteLine("You rolled a 1! You got 0 points.");
+                    break;
+                }
+                sum += roll;
+                Console.WriteLine($"You rolled a {roll}! Continue (yes/no)?");
+                ans = Console.ReadLine();
+            }
+            while (ans == "yes");            
+            if(roll != 1)
+                Console.WriteLine($"You got {sum} points!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Loops/Loops/Exercise9_RollTwoDice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9_RollTwoDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter sum (2 - 12):");
+            int desired = Int32.Parse(Console.ReadLine());
+            int sum = 0;
+            var rnd = new Random();
+
+            do
+            {
+                int roll1 = rnd.Next(1, 6);
+                int roll2 = rnd.Next(1, 6);
+                sum = roll1 + roll2;
+                Console.WriteLine($"{roll1} and {roll2} = {sum}");
+
+            } while (sum != desired);
+            Console.ReadKey();
+        }
+    }
+}
