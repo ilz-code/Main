ce829bb69ab20b138074dae9a7577de6c8a8a446
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-07-29 21:57:58 +0300

    Starting

---
+++ Collections/Exercise1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+        /**
+           * Origination:
+           * Audi -> Germany
+           * BMW -> Germany
+           * Honda -> Japan
+           * Mercedes -> Germany
+           * VolksWagen -> Germany
+           * Tesla -> USA
+           */
+
+        private static void Main(string[] args)
+        {
+            string[] array = { "Audi", "BMW", "Honda", "Mercedes", "VolksWagen", "Tesla" };
+
+            //todo - replace array with an List and print out the results
+
+            //todo - replace array with a HashSet and print out the results
+
+            //todo - replace array with a Dictionary (use brand as key and origination as value) and print out the results
+            var list = new List<string>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                list.Add(array[i]);
+            }
+            Console.WriteLine(String.Join(", ", array));
+
+            HashSet<string> hset = new HashSet<string>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                hset.Add(array[i]);
+            }
+            Console.WriteLine(String.Join("; ", hset));
+
+            string[] arc = { "Germany", "Germany", "Japan", "Germany", "Germany", "USA" };
+
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            for (int i = 0; i < arc.Length; i++)
+            {
+                dict.Add(array[i], arc[i]);
+            }
+            Console.WriteLine($"BMW is produced in {dict["BMW"]}");
+           // foreach (KeyValuePair car in dict)
+            {
+
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Collections/WordCount/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            try
+            {
+                 var tekst = File.ReadAllText("..\\..\\lear.txt");
+                //StreamReader sr = new StreamReader("..\\..\\lear.txt");
+                line = sr.ReadLine();
+
+                //C: \\Users\ilzep\Documents\GitHub\Main\Collections\WordCount\
+
+                //while (line != null)
+                //{
+                //write the lie to console window
+                Console.WriteLine(line);
+                //Read the next line
+                // line = sr.ReadLine();
+                // }
+
+                //sr.Close();
+                //string[] words = String.Split(" ",);
+                Console.ReadKey();
+            }
+            finally
+            {
+                Console.WriteLine("xx");
+                Console.ReadKey();
+            }
+            Console.ReadKey();
+        }
+    }
+}
