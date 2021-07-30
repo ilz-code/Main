3aa6bb53e4d4312cee25db3bbbf5d1aed8561cb9
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-07-30 22:25:08 +0300

    Ex4-7

--- Collections/FlightPlanner/Program.cs
+++ Collections/FlightPlanner/Program.cs
@@ -1,23 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FlightPlanner
 {
     class Program
     {
-        private const string Path = "../FlightPlanner/flights.txt";
-
+        private const string Path = "..\\..\\flights.txt";
         private static void Main(string[] args)
         {
             var readText = File.ReadAllLines(Path);
+
+            List<Flight> flights = new List<Flight>();
+            HashSet<string> city = new HashSet<string>();
+            string[] cities;
+            List<string> flightPoints = new List<string>();
+
             foreach (var s in readText)
             {
-                Console.WriteLine(s);
+                cities = s.Split('>');
+                string a = cities[0].Substring(0, cities[0].Length - 2);
+                string b = cities[1].Substring(1);
+                flights.Add(new Flight { CityFrom = a, CityTo = b });
             }
+
+            Console.WriteLine("Flights are possible from:");
+            foreach (Flight flight in flights)
+            {
+                city.Add(flight.CityFrom);
+            }
+            Console.WriteLine(String.Join("\n", city));
+            Console.WriteLine("\n Enter city from which flay:");
+            string point = Console.ReadLine();
+            string firstPoint = point;
+            do
+            {
+                flightPoints.Add(point);
+                Console.WriteLine($"\n From {point} flights are possible to:");
+                foreach (Flight flight in flights)
+                {
+                    if (flight.CityFrom == point)
+                        Console.WriteLine(flight.CityTo);
+                }
+                Console.WriteLine("\n Enter your destination");
+                point = Console.ReadLine();
+            }
+            while (point != firstPoint);
+
+            Console.WriteLine("\n Your round trip will be: " + String.Join(" -> ", flightPoints) + " -> " + point);
+
+            Console.ReadKey();
         }
+    }
+    public class Flight
+    {
+        public string CityFrom { get; set; }
+        public string CityTo { get; set; }
     }
 }
---
+++ Collections/HappyNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HappyNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number: ");
+            string number = Console.ReadLine();
+            List<char> chars = new List<char>();
+            List<double> nums = new List<double>();
+            do
+            {
+                chars.Clear();
+                nums.Clear();
+                chars = number.ToList<char>();
+                foreach (char c in chars)
+                {
+                    nums.Add(Math.Pow((Convert.ToInt32(c) - 48), 2));
+                }
+                number = Convert.ToString(nums.Sum());               
+            }
+            while (number.Length > 1);
+
+            if (number == "1")
+                Console.WriteLine("Number is happy");
+            else
+                Console.WriteLine("Number is not happy");
+
+            Console.ReadKey();
+        }
+    }
+}
