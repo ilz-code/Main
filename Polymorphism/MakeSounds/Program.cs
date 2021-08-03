a2b5e1059f2d4e2fe46f329d1e6128187991e72c
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-08-03 22:42:24 +0300

    MakeSounds

--- Polymorphism/MakeSounds/Program.cs
+++ Polymorphism/MakeSounds/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MakeSounds
 {
@@ -6,7 +7,29 @@ namespace MakeSounds
     {
         private static void Main(string[] args)
         {
-            
+            List<ISound> sounders = new List<ISound>();
+
+            Parrot john = new Parrot();
+            sounders.Add(john);
+            Parrot ella = new Parrot();
+            sounders.Add(ella);
+            Radio swh = new Radio();
+            sounders.Add(swh);
+            Radio bbc = new Radio();
+            sounders.Add(bbc);
+            Firework fiery = new Firework();
+            sounders.Add(fiery);
+            Firework bang = new Firework();
+            sounders.Add(bang);
+
+            Random rnd = new Random();
+            int i=0;
+            while ( i < 15)
+            {
+                sounders[rnd.Next(sounders.Count)].PlaySound();
+                i++;
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ Polymorphism/MakeSounds/Radio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakeSounds
+{
+    class Radio: ISound
+    {
+        public Radio()
+        {
+
+        }
+
+        public void PlaySound()
+        {
+            Console.WriteLine("Blahblahblah");
+        }
+    }
+}
