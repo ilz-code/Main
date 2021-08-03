9df705e7cf905e0163e904013a55cedef892b785
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-08-03 15:59:17 +0300

    AdApp

--- Polymorphism/AdApp/Program.cs
+++ Polymorphism/AdApp/Program.cs
@@ -8,10 +8,12 @@ namespace AdApp
         {
             var c = new Campaign();
             c.AddAdvert(new Advert(1000));
-            c.AddAdvert(new Hoarding(500, 7, 200));
-            c.AddAdvert(new NewspaperAd(0, 30, 20));
-            c.AddAdvert(new TVAd(50000, 1000, 30, true));
+            //c.AddAdvert(new Hoarding(500, 7, 200));
+            //c.AddAdvert(new NewspaperAd(0, 30, 20));
+            //c.AddAdvert(new TVAd(50000, 1000, 30, true));
             Console.WriteLine(c);
+
+            Console.ReadKey();
         }
     }
 }
