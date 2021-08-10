d4f9f5dd9c0fedb88c9fa1091898aeb12077c607
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-08-10 11:43:18 +0300

    Started

---
+++ Tests/Exercise6.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Exercise6.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
