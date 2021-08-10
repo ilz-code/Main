fd45e2b9516f0bf5a61e6cebaa9e4458cb621dc9
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-08-10 20:57:19 +0300

    Update ArrayElementTest.cs

--- Tests/Exercise6.Tests/ArrayElementTest.cs
+++ Tests/Exercise6.Tests/ArrayElementTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
 
 namespace Exercise6.Tests
 {
@@ -8,11 +9,28 @@ namespace Exercise6.Tests
     {
         
         [TestMethod]
-        public void TestMethod1()
+        public void FindElement_ArrayAndIndex_Element()
         {
             int[] firstArray = { 21, 8, 81, 50, 76, 6, 32, 46, 1, -7 };
             var result =ArrayElement.FindElement(firstArray, 9);
             Assert.AreEqual(-7, result);
         }
+
+        [TestMethod]
+        public void MakeArray_NewArray()
+        {
+            var arrayLength = ArrayElement.MakeArray().Length;
+            Assert.AreEqual(10, arrayLength);
+        }
+
+        [TestMethod]
+        public void CopyArray_oneArray_otherArray()
+        {
+            int[] firstArray = { 21, 8, 81, 50, 76, 6, 32, 46, 1, -7 };
+            int[] secondArray = ArrayElement.CopyArray(firstArray);
+            bool equals = firstArray.SequenceEqual(secondArray);
+            Assert.AreEqual(true, equals);
+        }
+
     }
 }
