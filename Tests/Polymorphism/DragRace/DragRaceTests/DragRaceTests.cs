059ac4d9a350054c5f2f901166b19862e3b939b7
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-08-13 08:36:48 +0300

    DRT

---
+++ Tests/Polymorphism/DragRace/DragRaceTests/DragRaceTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DragRaceTests
+{
+    [TestClass]
+    public class DragRaceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int result = DragRace.Dragrace.MakeCarsList().Count;
+        }
+    }
+}
---
+++ Tests/Polymorphism/DragRace/DragRaceTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DragRaceTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DragRaceTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ac1cf87f-e401-46d0-8bad-5358c125886f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
